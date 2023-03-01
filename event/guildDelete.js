bb7738801a3f4b0135149c230fd2be64c5131319
meliooff <antoniodu1333@gmail.com>
2023-03-01 15:24:00 +0100

    (~) Bug fixes

--- event/guildDelete.js
+++ event/guildDelete.js
@@ -4,14 +4,27 @@ module.exports = async (client,guild) => {
     var guilds = client.guilds.cache.size
     client.db.query(`DELETE FROM guild WHERE guildID = '${guild.id}';`)
 
-    get(`https://discord.com/api/v6/users/${guild.ownerId}`, { headers: { Authorization: `Bot ${client.config.token}` } }).then(data => { data.json().then(owner => {
-        client.guilds.cache.get(client.config.serverID).channels.cache.get(client.config.logChannel).send({embeds: [{
+    get(`https://discord.com/api/v6/users/${guild.ownerId}`, { headers: { Authorization: `Bot ${client.config.token}` } }).then(data => {
+        if(!data) { 
+            client.guilds.cache.get(client.config.serverID).channels.cache.get(client.config.logChannel).send({embeds: [{
                 color: 0xe24646,
                 thumbnail: { url: guild.iconURL({format: 'png'}) },
-                author: { name: `${guild.name} - ${guild.nameAcronym}`, icon_url: guild.iconURL(), url: `https://github.com/meliooff/naybor` },
-                description: `\`${owner.username}#${owner.discriminator}\` kicked me out of his/her server with **${guild.memberCount}** members.\nI'm now on **${guilds}** servers..`,
+                author: { name: `I didn't find data on the server`, icon_url: guild.iconURL(), url: `https://github.com/meliooff/naybor` },
+                description: `Someone kicked me out of his/her server.\nI'm now on **${guilds}** servers..`,
                 footer: { text: `ID : ${guild.id}` },
                 timestamp: new Date(),
-        }]}).catch(e=>console.log(e)) 
-})})
+            }]}).catch(e=>console.log(e));
+            return;
+        }
+        data.json().then(owner => {
+            client.guilds.cache.get(client.config.serverID).channels.cache.get(client.config.logChannel).send({embeds: [{
+                    color: 0xe24646,
+                    thumbnail: { url: guild.iconURL({format: 'png'}) },
+                    author: { name: `${guild.name} - ${guild.nameAcronym}`, icon_url: guild.iconURL(), url: `https://github.com/meliooff/naybor` },
+                    description: `\`${owner.username}#${owner.discriminator}\` kicked me out of his/her server with **${guild.memberCount}** members.\nI'm now on **${guilds}** servers..`,
+                    footer: { text: `ID : ${guild.id}` },
+                    timestamp: new Date(),
+            }]}).catch(e=>console.log(e)) 
+        })
+    })
 };
