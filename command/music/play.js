2be45e8761ddfd73e59f3a5060974d82fec8abdf
Mélio <47474006+meliooff@users.noreply.github.com>
2023-05-04 00:39:14 +0200

    ⚗️ Logging error

--- command/music/play.js
+++ command/music/play.js
@@ -40,6 +40,7 @@ module.exports={
                 }
             });
         } catch (e) {
+            console.log(e);
             return message.channel.send(client.language.ERROR[1]);
         }
     }
